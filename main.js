d7ee9877719877b89738fa17a6167667d96e0a6b
o-Spamton-o <160066769+o-Spamton-o@users.noreply.github.com>
2024-03-19 13:37:50 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+document.getElementById("btn1").addEventListener("click", answer1);
+
+function answer1() {
+  alert(`A blessing in disguise`);
+}
+document.getElementById("btn2").addEventListener("click", answer2);
+
+function answer2() {
+  alert(`A dime a dozen`);
+}
+
+document.getElementById("btn3").addEventListener("click", answer3);
+
+function answer3() {
+  alert(`Better late than never`);
+}
+
+document.getElementById("btn4").addEventListener("click", answer4);
+
+function answer4() {
+  alert(`No pain no gain`);
+}
+
+document.getElementById("btn5").addEventListener("click", answer5);
+
+function answer5() {
+  alert(`Time flies when you're having fun`);
+}
